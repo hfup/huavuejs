9624957964c65b1175ee410817a6305bc07c45df
huangfei <huangfeigo@qq.com>
2023-08-22 01:11:07 +0800

    updated api

--- src/hook/pagination.ts
+++ src/hook/pagination.ts
@@ -1,6 +1,6 @@
 import type {Ref} from "@vue/runtime-core";
 import {ref} from "@vue/runtime-core";
-import type {PaginationOption} from "../request";
+import type {JsonResult, PaginationOption} from "../request";
 import {post} from "../request";
 
 
@@ -8,8 +8,8 @@ interface PaginationHook<R,Q>{
     isLoading:Ref<boolean>,
     list:Ref<R[]>,
     total:Ref<number>,
-    changePage:(path:string,paginationOpt:PaginationOption<Q> | undefined) => Promise<void> // 切换页码
-    delItem:<K extends keyof R>(path:string,key:K,value:R[K]) => Promise<void> // 删除列表项
+    changePage:(path:string,paginationOpt:PaginationOption<Q> | undefined) => Promise<JsonResult<void>> // 切换页码
+    delItem:<K extends keyof R>(path:string,key:K,value:R[K]) => Promise<JsonResult<void>> // 删除列表项
 }
 
 function usePagination<R=any,Q=any>():PaginationHook<R,Q> {
@@ -20,19 +20,19 @@ function usePagination<R=any,Q=any>():PaginationHook<R,Q> {
     const changePage = async (path:string,paginationOpt:PaginationOption<Q> | undefined) => {
         let reqOpt = paginationOpt ?? {page_no:1,page_size:10}
         isLoading.value = true
-        post<{list:R[],total:number}>(path,reqOpt).then(res => {
-            if (res.err_code !== 0) {
-                throw new Error(res.message)
+        return post<{list:R[],total:number}>(path,reqOpt).then(res => {
+            if (res.err_code === 0) {
+                isLoading.value = false
+                list.value = res.data?.list??[]
+                total.value = res.data?.total??0
             }
-            isLoading.value = false
-            list.value = res.data?.list??[]
-            total.value = res.data?.total??0
+            return {err_code:res.err_code,message:res.message} as JsonResult<void>
         })
     }
 
-    const delItem = async <K extends keyof R>(path:string,key:K,value:R[K]) => {
+    const delItem = async <K extends keyof R>(path:string,key:K,value:R[K]):Promise<JsonResult<void>> => {
         isLoading.value = true
-        post(path,{[key]:value}).then(res => {
+        return await post<void>(path,{[key]:value}).then(res => {
             isLoading.value = false
             if (res.err_code === 0) {
                 let index = list.value.findIndex(item => item[key] === value)
@@ -40,6 +40,7 @@ function usePagination<R=any,Q=any>():PaginationHook<R,Q> {
                     list.value.splice(index,1)
                 }
             }
+            return res
         })
     }
 
